ee09eb7b04fd120ee0fabbea720ff0ccfd30f3ac
Mason, Joel <Joel.Mason@aesynt.com>
2016-01-14 08:14:37 -0500

    Project Scaffold

---
+++ src/ApiGateway/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ApiGateway")>]
+[<assembly: AssemblyProductAttribute("ApiGateway")>]
+[<assembly: AssemblyDescriptionAttribute("An API Gateway")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/ApiGateway.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module ApiGateway.Tests
+
+open ApiGateway
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
