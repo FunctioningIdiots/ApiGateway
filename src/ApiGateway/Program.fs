e8d1e12d27be889d91f7b504040e720c5bf15954
Korhnak, Dan <Daniel.Korhnak@aesynt.com>
2016-01-18 22:11:41 -0500

    mapped GETs to backend servers - contrived example

--- src/ApiGateway/Program.fs
+++ src/ApiGateway/Program.fs
@@ -15,22 +15,15 @@ let services =
         {Name="city"; Server="cityserver"; Port=8001us }
     ]
 
-let generateWebPart service = 
-    let createPathScanString = new PrintfFormat<_,_,_,_,_>(sprintf "%s/%s" service.Name "%s")
+let generateWebPart (service:service) : WebPart<'a> = 
+    let createPathScanString = new PrintfFormat<_,_,_,_,_>(sprintf "/%s/%s" service.Name "%s")
     let url segment = 
         sprintf "http://%s:%u/%s" service.Server service.Port segment
-    (GET >=> pathStarts (sprintf "/%s" service.Name) >=> pathScan createPathScanString (fun segment -> OK (url segment))) :> WebPart
+    GET >=> pathStarts (sprintf "/%s" service.Name) >=> pathScan createPathScanString (fun segment -> OK (url segment))
 
 [<EntryPoint>]
 let main argv =     
-    let routes = List.map generateWebPart services 
-    let app = choose [ 
-                routes
-                GET >=> pathStarts "/louisiana" >=> pathScan "%s" (fun segment -> OK (sprintf "http://otherserver:8001/%s" segment))
-                GET >=> pathScan "/%s" (fun segment -> OK segment) 
-                POST >=> pathScan "/%s" (fun segment -> OK segment) 
-                NOT_FOUND "No handlers found"]
-
+    let routes = (NOT_FOUND "No handlers found") :: (List.map generateWebPart services)
+    let app = choose (List.rev routes)
     startWebServer defaultConfig app
-
     0
